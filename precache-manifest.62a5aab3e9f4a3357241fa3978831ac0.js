ba6f4319b32f224a5def278303336ffa70901ed7
MikevPeeren <MikevPeeren@hotmail.com>
2020-03-06 16:37:08 +0100

    Updates

---
+++ precache-manifest.62a5aab3e9f4a3357241fa3978831ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32118bc8a5a429a713fefd420c35ee7f",
+    "url": "/Memory-Card-Game/index.html"
+  },
+  {
+    "revision": "7afd6d9693da734b4e39",
+    "url": "/Memory-Card-Game/static/css/main.ec689f9d.chunk.css"
+  },
+  {
+    "revision": "17bb362ca98d83c4a984",
+    "url": "/Memory-Card-Game/static/js/2.91e40da6.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Memory-Card-Game/static/js/2.91e40da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afd6d9693da734b4e39",
+    "url": "/Memory-Card-Game/static/js/main.2d558310.chunk.js"
+  },
+  {
+    "revision": "c2f2bb1271808f34d049",
+    "url": "/Memory-Card-Game/static/js/runtime-main.6573b51a.js"
+  }
+]);
