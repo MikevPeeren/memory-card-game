fb140189e2a7ec9913453fc836a08068a77407a5
MikevPeeren <MikevPeeren@hotmail.com>
2020-03-08 13:43:44 +0100

    Removed TS Ignore

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -24,7 +24,6 @@ class Game extends Component<{}, GameState> {
 	constructor(props: {}) {
 		super(props);
 		this.state = {
-			//@ts-ignore
 			flippableSquares: flippableSquaresJSON,
 			flippedSquares: [],
 			progressBarValue: 0,
