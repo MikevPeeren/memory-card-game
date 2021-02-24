5858f7d81ecdaf7efa98bac6f51a8537cac22139
MikevPeeren <MikevPeeren@hotmail.com>
2021-02-24 19:30:18 +0100

    :bug: fix for prettier eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier/@typescript-eslint',
-		'plugin:prettier/recommended',
-	],
-	plugins: ['react', '@typescript-eslint', 'prettier', 'jsx-a11y'],
-	env: {
-		browser: true,
-		jest: true,
-	},
-	rules: {
-		'prettier/prettier': ['error', { singleQuote: true }],
-		'react/jsx-uses-react': 'error',
-		'react/jsx-uses-vars': 'error',
-		// disable the rule for all files
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-	},
-	settings: {
-		react: {
-			pragma: 'React',
-			version: 'detect',
-		},
-	},
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'jsx-a11y'],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    // disable the rule for all files
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 };
